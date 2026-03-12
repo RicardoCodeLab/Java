eb7ad954cf4c3d81b2c8576a8023c1151dc23f54
Ricardo Alonso Temoche Soriano <sorianoalonso83@gmail.com>
2026-03-11 22:40:14 -0500

    Delete src/POO/Ejercicio6_POO directory

--- src/POO/Ejercicio6_POO/MainEjercicio6.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio6_POO;
-public class MainEjercicio6 {
-    public static void main(String[] args) {
-        Persona Alonso=new Persona("Ricardo Alonso",16,'H');
-        System.out.println(Alonso);
-        Persona Adriana=new Persona("Adriana Soriano",41,'F');
-        System.out.println(Adriana);
-    }
-}
